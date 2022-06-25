02e40ad9801cb240964ca934a3498199c57cb922
milicabacic <milicabacic99@gmail.com>
2022-06-25 14:34:34 +0200

    napravljena prva stranica

---
+++ footer.php
@@ -0,0 +1,24 @@
+</body>
+<footer id="footer" class="footer">
+  <div class="container text-center">
+
+  <h4>Za više informacija o auto školi, možete da posetite naš sajt ili nas zapratite na društvenim mrežama </h4>
+  <ul class="social-links">
+  <!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+
+<li><a href="#link"><i class="fa fa-facebook"></i></a></li>
+<li><a href="#link"><i class="fa fa-twitter"></i></a></li>  
+<li><a href="#link"><i class="fa fa-google"></i></a></li>
+
+ 
+</body>
+</html>
+</ul>
+  Auto škola "BIS MOTIV" — Južni bulvear 15, Beograd - Vračar
+</div>
+</footer>
---
+++ index.php
@@ -0,0 +1,74 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Autoskola Bis Motiv</title>
+  <link rel="icon" href="img/logo.png">
+  <meta name="description" content="">
+  <meta name="keywords" content="">
+
+  <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Open+Sans|Candal|Alegreya+Sans">
+  <link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
+  <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="css/imagehover.min.css">
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+  <link rel="stylesheet" type="text/css" href="css/slike.css">
+
+
+</head>
+
+<body>
+  <?php include 'header.php'; ?>
+
+
+  <section id="feature" class="section-padding">
+    <div class="container">
+      <div class="row">
+        <div class="header-section text-center">
+
+          <div class="col-md-12 text-center">
+            <img src="img/logo.png" class="img img-square"> <br><br>
+          </div>
+          <p>Naša misija je da Vas osposobimo da bezbedno i sigurno učestvujete u saobraćaju. Kroz celu obuku Vas učimo da pravilno,
+            bezbedno i sigurno upravljate vozilom u svim saobaćajnim situacijama i da stečeno teorijsko znajnje primenjujete u
+            saobraćaju. <br> <br>
+            Naša vizija je da imamo aktivnu i odlučujuću ulogu u poboljšanju kvaliteta vožnje, upravljanju i ublažavanju rizika u
+            saobraćaju i stvaranju novih vrednosti. Naš cilj je da svaki naš kandidat bude primer sigurnog i bezbednog učesnika u
+            saobraćaju.
+          </p>
+          <hr class="bottom-line">
+
+          <div class="col-md-12 text-center">
+            <div class="row">
+              <div class="column">
+                <img src="img/ford.png" style="width:100%">
+              </div>
+              <div class="column">
+                <img src="img/skola.png" style="width:100%">
+              </div>
+              <div class="column">
+                <img src="img/poligon.png" style="width:100%">
+              </div>
+              <div class="column">
+                <img src="img/bmw.png" style="width:100%">
+              </div>
+            </div>
+
+          </div>
+
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <?php include 'footer.php'; ?>
+
+</body>
+
+</html>
